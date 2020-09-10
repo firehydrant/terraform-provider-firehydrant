a845cd000dc16f3c411196c3678f7c1b4b121be5
Robert Ross <robert@firehydrant.io>
2020-09-10 14:34:07 -0400

    Simple mod init with terraform sdk

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/firehydrant/terraform-provider-firehydrant
+
+go 1.15
+
+require github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.1
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return nil
+		},
+	})
+}
