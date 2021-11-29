6f48186720f0137eac0c24e93eec08dd6d9583c3
John Paul Ashenfelter <john@ashenfelter.com>
2021-11-29 12:26:39 -0500

    Finish the runbooks definition using slugs instead of UUIDs

--- examples/main.tf
+++ examples/main.tf
@@ -16,15 +16,29 @@ resource "firehydrant_environment" "production" {
     name = "Production"
 }
 
-resource "firehydrant_team" "firefighters" {
-  name = "Firefighters"
-}
-
 data "firehydrant_runbook_action" "slack_channel" {
   integration_slug = "slack"
   slug = "create_incident_channel"
   type = "incident"
 }
+
+data "firehydrant_runbook_action" "notify_channel" {
+  integration_slug = "slack"
+  slug = "notify_channel"
+  type = "incident"
+}
+data "firehydrant_runbook_action" "notify_channel_custom" {
+  integration_slug = "slack"
+  slug = "notify_incident_channel_custom_message"
+  type = "incident"
+}
+
+data "firehydrant_runbook_action" "create_incident_ticket" {
+  integration_slug = "patchy"
+  slug = "create_incident_ticket"
+  type = "incident"
+}
+
 
 data "firehydrant_runbook_action" "archive_channel" {
   integration_slug = "slack"
@@ -54,6 +68,19 @@ resource "firehydrant_runbook" "default" {
         repeats   = false
     }
     steps {
+        action_id = data.firehydrant_runbook_action.notify_channel_custom.id
+        automatic = true
+        config    = {
+            "message" = <<EOT
+                Here's the documentation on successfully running an incident with FireHydrant's Slack bot: https://help.firehydrant.io/en/articles/3050697-incident-response-w-slack
+
+                Don't worry all of your messages and actions here are tracked into your incident on the FireHydrant UI.
+            EOT
+        }
+        name      = "Incident Preamble"
+        repeats   = false
+    }
+    steps {
         action_id = data.firehydrant_runbook_action.email_notification.id
         automatic = true
         config    = {
@@ -63,6 +90,41 @@ resource "firehydrant_runbook" "default" {
         name      = "Email stakeholders"
         repeats   = false
     }
+    steps {
+        action_id = data.firehydrant_runbook_action.notify_channel.id
+        automatic = true
+        config    = {
+            "channels" = "#fh-incidents"
+        }
+        name      = "Notify incidents channel that a new incident has been opened"
+        repeats   = false
+    }
+    steps {
+        action_id = "b60654c8-2c15-4a25-b5e9-2ac2884f0b9b"
+        automatic = true
+        config    = {
+            "ticket_description" = "{{ incident.description }}"
+            "ticket_summary"     = "{{ incident.name }}"
+        }
+        name      = "Create an incident ticket in Jira"
+        repeats   = false
+    }
+    steps {
+        action_id = data.firehydrant_runbook_action.notify_channel_custom.id
+        automatic = true
+        config    = {
+            "message" = <<EOT
+                Please check-in with your current status on this {{ incident.severity }} incident
+
+                ```
+                /firehydrant add note I'm calculating the power required by the flux capacitor
+                ```
+            EOT
+        }
+        name      = "Remind Slack channel to update stakeholders"
+        repeats   = false
+    }
+
     steps {
         action_id = data.firehydrant_runbook_action.archive_channel.id
         automatic = true
