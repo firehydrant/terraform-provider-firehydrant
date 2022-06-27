10d76fc5d3858dcaacd6c191b9938eb79a97dcf6
Krista LaFentres <lafentres@users.noreply.github.com>
2022-06-27 15:42:40 -0500

    d/runbook: Refactor for clarity and consistency

--- provider/runbook_data.go
+++ provider/runbook_data.go
@@ -14,15 +14,18 @@ func dataSourceRunbook() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataFireHydrantRunbook,
 		Schema: map[string]*schema.Schema{
+			// Required
 			"id": {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"name": {
+
+			// Computed
+			"description": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"description": {
+			"name": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -31,27 +34,31 @@ func dataSourceRunbook() *schema.Resource {
 }
 
 func dataFireHydrantRunbook(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	ac := m.(firehydrant.Client)
-	serviceID := d.Get("id").(string)
+	// Get the API client
+	firehydrantAPIClient := m.(firehydrant.Client)
 
-	r, err := ac.Runbooks().Get(ctx, serviceID)
+	// Get the runbook
+	runbookID := d.Get("id").(string)
+	runbookResponse, err := firehydrantAPIClient.Runbooks().Get(ctx, runbookID)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	var ds diag.Diagnostics
-	svc := map[string]string{
-		"name":        r.Name,
-		"description": r.Description,
+	// Gather values from API response
+	attributes := map[string]interface{}{
+		"description": runbookResponse.Description,
+		"name":        runbookResponse.Name,
 	}
 
-	for key, val := range svc {
-		if err := d.Set(key, val); err != nil {
+	// Set the data source attributes to the values we got from the API
+	for key, value := range attributes {
+		if err := d.Set(key, value); err != nil {
 			return diag.FromErr(err)
 		}
 	}
 
-	d.SetId(r.ID)
+	// Set the runbook's ID in state
+	d.SetId(runbookResponse.ID)
 
-	return ds
+	return diag.Diagnostics{}
 }
