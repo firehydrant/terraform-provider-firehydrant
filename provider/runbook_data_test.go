c6e9ebda68c4ba490c1d72ab2b82f2274cb48297
Krista LaFentres (she/her) <lafentres@users.noreply.github.com>
2022-06-29 11:20:48 -0500

    Add tests for r/runbook and d/runbook (#70)

---
+++ provider/runbook_data_test.go
@@ -0,0 +1,102 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccRunbookDataSource_basic(t *testing.T) {
+	rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testFireHydrantIsSetup(t) },
+		ProviderFactories: defaultProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRunbookDataSourceConfig_basic(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.firehydrant_runbook.test_runbook", "id"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_runbook.test_runbook", "name", fmt.Sprintf("test-runbook-%s", rName)),
+				),
+			},
+		},
+	})
+}
+
+func TestAccRunbookDataSource_allAttributes(t *testing.T) {
+	rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testFireHydrantIsSetup(t) },
+		ProviderFactories: defaultProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRunbookDataSourceConfig_allAttributes(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.firehydrant_runbook.test_runbook", "id"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_runbook.test_runbook", "name", fmt.Sprintf("test-runbook-%s", rName)),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_runbook.test_runbook", "description", fmt.Sprintf("test-description-%s", rName)),
+				),
+			},
+		},
+	})
+}
+
+func testAccRunbookDataSourceConfig_basic(rName string) string {
+	return fmt.Sprintf(`
+data "firehydrant_runbook_action" "create_incident_channel" {
+  slug             = "create_incident_channel"
+  integration_slug = "slack"
+  type             = "incident"
+}
+
+resource "firehydrant_runbook" "test_runbook" {
+  name        = "test-runbook-%s"
+  type        = "incident"
+
+  steps {
+    name      = "Create Incident Channel"
+    action_id = data.firehydrant_runbook_action.create_incident_channel.id
+    config = {
+      channel_name_format = "-inc-{{ number }}"
+    }
+  }
+}
+
+data "firehydrant_runbook" "test_runbook" {
+  id = firehydrant_runbook.test_runbook.id
+}`, rName)
+}
+
+func testAccRunbookDataSourceConfig_allAttributes(rName string) string {
+	return fmt.Sprintf(`
+data "firehydrant_runbook_action" "create_incident_channel" {
+  slug             = "create_incident_channel"
+  integration_slug = "slack"
+  type             = "incident"
+}
+
+resource "firehydrant_runbook" "test_runbook" {
+  name        = "test-runbook-%s"
+  type        = "incident"
+  description = "test-description-%s"
+
+  steps {
+    name      = "Create Incident Channel"
+    action_id = data.firehydrant_runbook_action.create_incident_channel.id
+    config = {
+      channel_name_format = "-inc-{{ number }}"
+    }
+  }
+}
+
+data "firehydrant_runbook" "test_runbook" {
+  id = firehydrant_runbook.test_runbook.id
+}`, rName, rName)
+}
