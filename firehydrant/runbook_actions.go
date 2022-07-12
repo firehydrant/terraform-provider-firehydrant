33f0ed2f41ad553e6e4a3cbbb0eaef4340a9b7df
Krista LaFentres <lafentres@users.noreply.github.com>
2022-07-12 14:38:02 -0500

    Add full list of integration slugs and runbook action slugs

--- firehydrant/runbook_actions.go
+++ firehydrant/runbook_actions.go
@@ -9,6 +9,66 @@ import (
 	"github.com/pkg/errors"
 )
 
+// RunbookActionIntegrationSlug represents the integration slug of the integration
+// associated with a runbook action
+type RunbookActionIntegrationSlug string
+
+// List of valid integration slugs
+const (
+	RunbookActionIntegrationSlugConfluenceCloud RunbookActionIntegrationSlug = "confluence_cloud"
+	RunbookActionIntegrationSlugFireHydrant     RunbookActionIntegrationSlug = "patchy"
+	RunbookActionIntegrationSlugFireHydrantNunc RunbookActionIntegrationSlug = "nunc"
+	RunbookActionIntegrationSlugGiphy           RunbookActionIntegrationSlug = "giphy"
+	RunbookActionIntegrationSlugGoogleDocs      RunbookActionIntegrationSlug = "google_docs"
+	RunbookActionIntegrationSlugGoogleMeet      RunbookActionIntegrationSlug = "google_meet"
+	RunbookActionIntegrationSlugJiraCloud       RunbookActionIntegrationSlug = "jira_cloud"
+	RunbookActionIntegrationSlugJiraServer      RunbookActionIntegrationSlug = "jira_server"
+	RunbookActionIntegrationSlugMicrosoftTeams  RunbookActionIntegrationSlug = "microsoft_teams"
+	RunbookActionIntegrationSlugOpsgenie        RunbookActionIntegrationSlug = "opsgenie"
+	RunbookActionIntegrationSlugPagerDuty       RunbookActionIntegrationSlug = "pager_duty"
+	RunbookActionIntegrationSlugShortcut        RunbookActionIntegrationSlug = "shortcut"
+	RunbookActionIntegrationSlugSlack           RunbookActionIntegrationSlug = "slack"
+	RunbookActionIntegrationSlugStatuspage      RunbookActionIntegrationSlug = "statuspage"
+	RunbookActionIntegrationSlugVictorOps       RunbookActionIntegrationSlug = "victorops"
+	RunbookActionIntegrationSlugWebex           RunbookActionIntegrationSlug = "webex"
+	RunbookActionIntegrationSlugZoom            RunbookActionIntegrationSlug = "zoom"
+)
+
+// RunbookActionSlug represents the runbook action's slug
+type RunbookActionSlug string
+
+// List of valid action slugs
+const (
+	RunbookActionSlugAddServicesRelatedToFunctionality  RunbookActionSlug = "add_services_related_to_functionality"
+	RunbookActionSlugAddTaskList                        RunbookActionSlug = "add_task_list"
+	RunbookActionSlugArchiveIncidentChannel             RunbookActionSlug = "archive_incident_channel"
+	RunbookActionSlugAssignARole                        RunbookActionSlug = "assign_a_role"
+	RunbookActionSlugAssignATeam                        RunbookActionSlug = "assign_a_team"
+	RunbookActionSlugAttachARunbook                     RunbookActionSlug = "attach_a_runbook"
+	RunbookActionSlugCreateGoogleMeetLink               RunbookActionSlug = "create_google_meet_link"
+	RunbookActionSlugCreateIncidentChannel              RunbookActionSlug = "create_incident_channel"
+	RunbookActionSlugCreateIncidentIssue                RunbookActionSlug = "create_incident_issue"
+	RunbookActionSlugCreateIncidentTicket               RunbookActionSlug = "create_incident_ticket"
+	RunbookActionSlugCreateMeeting                      RunbookActionSlug = "create_meeting"
+	RunbookActionSlugCreateNewOpsgenieIncident          RunbookActionSlug = "create_new_opsgenie_incident"
+	RunbookActionSlugCreateNewPagerDutyIncident         RunbookActionSlug = "create_new_pager_duty_incident"
+	RunbookActionSlugCreateNunc                         RunbookActionSlug = "create_nunc"
+	RunbookActionSlugCreateStatuspage                   RunbookActionSlug = "create_statuspage"
+	RunbookActionSlugEmailNotification                  RunbookActionSlug = "email_notification"
+	RunbookActionSlugExportRetrospective                RunbookActionSlug = "export_retrospective"
+	RunbookActionSlugFreeformText                       RunbookActionSlug = "freeform_text"
+	RunbookActionSlugIncidentChannelGif                 RunbookActionSlug = "incident_channel_gif"
+	RunbookActionSlugIncidentUpdate                     RunbookActionSlug = "incident_update"
+	RunbookActionSlugNotifyChannel                      RunbookActionSlug = "notify_channel"
+	RunbookActionSlugNotifyChannelCustomMessage         RunbookActionSlug = "notify_channel_custom_message"
+	RunbookActionSlugNotifyIncidentChannelCustomMessage RunbookActionSlug = "notify_incident_channel_custom_message"
+	RunbookActionSlugScript                             RunbookActionSlug = "script"
+	RunbookActionSlugSendWebhook                        RunbookActionSlug = "send_webhook"
+	RunbookActionSlugSetLinkedAlertsStatus              RunbookActionSlug = "set_linked_alerts_status"
+	RunbookActionSlugUpdateStatuspage                   RunbookActionSlug = "update_statuspage"
+	RunbookActionSlugVictorOpsCreateNewIncident         RunbookActionSlug = "victorops_create_new_incident"
+)
+
 type RunbookActionsResponse struct {
 	Actions []RunbookAction `json:"data"`
 }
