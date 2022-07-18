e1d1289c39c52050fd31e691a9b2d92bfb6eca91
Krista LaFentres <lafentres@users.noreply.github.com>
2022-07-18 11:32:31 -0500

    d/runbook: Update test configs to account for steps config type change in r/runbook

--- provider/runbook_data_test.go
+++ provider/runbook_data_test.go
@@ -58,15 +58,16 @@ data "firehydrant_runbook_action" "create_incident_channel" {
 }
 
 resource "firehydrant_runbook" "test_runbook" {
-  name        = "test-runbook-%s"
-  type        = "incident"
+  name = "test-runbook-%s"
+  type = "incident"
 
   steps {
     name      = "Create Incident Channel"
     action_id = data.firehydrant_runbook_action.create_incident_channel.id
-    config = {
+
+    config = jsonencode({
       channel_name_format = "-inc-{{ number }}"
-    }
+    })
   }
 }
 
@@ -96,9 +97,10 @@ resource "firehydrant_runbook" "test_runbook" {
   steps {
     name      = "Create Incident Channel"
     action_id = data.firehydrant_runbook_action.create_incident_channel.id
-    config = {
+
+    config = jsonencode({
       channel_name_format = "-inc-{{ number }}"
-    }
+    })
   }
 }
 
