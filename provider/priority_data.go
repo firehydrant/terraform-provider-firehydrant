155eaaba101f846046ee0b2c9289e0db000109c0
Krista LaFentres <lafentres@users.noreply.github.com>
2022-07-26 11:53:47 -0500

    d/priority: Refactor to use refactored API client and add logging

--- provider/priority_data.go
+++ provider/priority_data.go
@@ -2,9 +2,11 @@ package provider
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/firehydrant/terraform-provider-firehydrant/firehydrant"
 
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -18,7 +20,7 @@ func dataSourcePriority() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			
+
 			// Computed
 			"default": {
 				Type:     schema.TypeBool,
@@ -35,12 +37,15 @@ func dataSourcePriority() *schema.Resource {
 func dataFireHydrantPriority(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	// Get the API client
 	firehydrantAPIClient := m.(firehydrant.Client)
-	
+
 	// Get the priority
 	slug := d.Get("slug").(string)
-	priorityResponse, err := firehydrantAPIClient.GetPriority(ctx, slug)
+	tflog.Debug(ctx, fmt.Sprintf("Read priority: %s", slug), map[string]interface{}{
+		"id": slug,
+	})
+	priorityResponse, err := firehydrantAPIClient.Priorities().Get(ctx, slug)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.Errorf("Error reading priority %s: %v", slug, err)
 	}
 
 	// Gather values from API response
@@ -52,7 +57,7 @@ func dataFireHydrantPriority(ctx context.Context, d *schema.ResourceData, m inte
 	// Set the data source attributes to the values we got from the API
 	for key, val := range attributes {
 		if err := d.Set(key, val); err != nil {
-			return diag.FromErr(err)
+			return diag.Errorf("Error setting %s for priority %s: %v", key, slug, err)
 		}
 	}
 
