cee93e15aad5650beb8b7b116f2f21f3f8fa797c
Jeff Zellner <jzellner@firehydrant.com>
2023-11-01 11:01:14 -0600

    use dynamic priority name

--- provider/priority_data_test.go
+++ provider/priority_data_test.go
@@ -2,22 +2,26 @@ package provider
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccPriorityDataSource_basic(t *testing.T) {
+	rSlug := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testFireHydrantIsSetup(t) },
 		ProviderFactories: defaultProviderFactories(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPriorityDataSourceConfig_basic(),
+				Config: testAccPriorityDataSourceConfig_basic(rSlug),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.firehydrant_priority.test_priority", "id"),
 					resource.TestCheckResourceAttr(
-						"data.firehydrant_priority.test_priority", "slug", "TESTPRIORITY"),
+						"data.firehydrant_priority.test_priority", "slug", fmt.Sprintf("TESTPRIORITY%s", rSlug)),
 					resource.TestCheckResourceAttr(
 						"data.firehydrant_priority.test_priority", "description", "test-description"),
 					resource.TestCheckResourceAttr(
@@ -28,15 +32,15 @@ func TestAccPriorityDataSource_basic(t *testing.T) {
 	})
 }
 
-func testAccPriorityDataSourceConfig_basic() string {
-	return fmt.Sprintln(`
+func testAccPriorityDataSourceConfig_basic(rSlug string) string {
+	return fmt.Sprintf(`
 resource "firehydrant_priority" "test_priority" {
-  slug        = "TESTPRIORITY"
+  slug        = "TESTPRIORITY%s"
   description = "test-description"
   default     = false
 }
 
 data "firehydrant_priority" "test_priority" {
   slug = firehydrant_priority.test_priority.id
-}`)
+}`, rSlug)
 }
