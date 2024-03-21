4af30f64f7909b7a5e2af1b25e30ab8244432464
Connor Teague <connor.a.teague@gmail.com>
2024-03-21 09:08:59 -0500

    update with formatting fixes

--- firehydrant/slack_channels.go
+++ firehydrant/slack_channels.go
@@ -58,9 +58,9 @@ func (c *RESTSlackChannelsClient) Get(ctx context.Context, params SlackChannelPa
 	if channelCount := len(channels.Channels); channelCount > 1 {
 		// "at least" because it may paginate.
 		tflog.Error(ctx, "found more than one Slack channel", map[string]interface{}{
-			"id":   params.ID,
-			"name": params.Name,
-			"found":      channelCount,
+			"id":    params.ID,
+			"name":  params.Name,
+			"found": channelCount,
 		})
 		for _, channel := range channels.Channels {
 			tflog.Error(ctx, "found Slack channel", map[string]interface{}{
