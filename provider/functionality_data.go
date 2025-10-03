9ac6076dc487ca0266857717c49b0db6537e45d1
Jonah Stewart <jstewart@firehydrant.com>
2025-10-03 18:18:35 -0400

    update functionalities

--- provider/functionality_data.go
+++ provider/functionality_data.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/firehydrant/terraform-provider-firehydrant/firehydrant"
 
+	"github.com/firehydrant/firehydrant-go-sdk/models/sdkerrors"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -41,38 +42,76 @@ func dataSourceFunctionality() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
+			"owner_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"team_ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"auto_add_responding_team": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 		},
 	}
 }
 
 func dataFireHydrantFunctionality(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	// Get the API client
-	firehydrantAPIClient := m.(firehydrant.Client)
+	client := m.(*firehydrant.APIClient)
 
 	// Get the functionality
 	functionalityID := d.Get("functionality_id").(string)
 	tflog.Debug(ctx, fmt.Sprintf("Read functionality: %s", functionalityID), map[string]interface{}{
 		"id": functionalityID,
 	})
-	functionalityResponse, err := firehydrantAPIClient.Functionalities().Get(ctx, functionalityID)
+	functionalityResponse, err := client.Sdk.CatalogEntries.GetFunctionality(ctx, functionalityID)
 	if err != nil {
+		if sdkErr, ok := err.(*sdkerrors.SDKError); ok && sdkErr.StatusCode == 404 {
+			return diag.Errorf("Functionality %s not found", functionalityID)
+		}
 		return diag.Errorf("Error reading functionality %s: %v", functionalityID, err)
 	}
 
 	// Gather values from API response
 	attributes := map[string]interface{}{
-		"name":        functionalityResponse.Name,
-		"description": functionalityResponse.Description,
-		"labels":      functionalityResponse.Labels,
+		"name":                     *functionalityResponse.Name,
+		"description":              *functionalityResponse.Description,
+		"labels":                   make(map[string]interface{}), // Labels handling TBD - empty for now
+		"auto_add_responding_team": *functionalityResponse.AutoAddRespondingTeam,
 	}
 
+	// Process service IDs
 	serviceIDs := make([]string, 0)
 	for _, service := range functionalityResponse.Services {
-		serviceIDs = append(serviceIDs, service.ID)
+		if service.ID != nil {
+			serviceIDs = append(serviceIDs, *service.ID)
+		}
 	}
 	attributes["service_ids"] = serviceIDs
 
-	// Set the resource attributes to the values we got from the API
+	// Process owner
+	var ownerID string
+	if functionalityResponse.Owner != nil && functionalityResponse.Owner.ID != nil {
+		ownerID = *functionalityResponse.Owner.ID
+	}
+	attributes["owner_id"] = ownerID
+
+	// Process team IDs
+	var teamIDs []interface{}
+	for _, team := range functionalityResponse.Teams {
+		if team.ID != nil {
+			teamIDs = append(teamIDs, *team.ID)
+		}
+	}
+	attributes["team_ids"] = teamIDs
+
+	// Set the data source attributes to the values we got from the API
 	for key, value := range attributes {
 		if err := d.Set(key, value); err != nil {
 			return diag.Errorf("Error setting %s for functionality %s: %v", key, functionalityID, err)
@@ -80,7 +119,7 @@ func dataFireHydrantFunctionality(ctx context.Context, d *schema.ResourceData, m
 	}
 
 	// Set the functionality's ID in state
-	d.SetId(functionalityResponse.ID)
+	d.SetId(*functionalityResponse.ID)
 
 	return diag.Diagnostics{}
 }
