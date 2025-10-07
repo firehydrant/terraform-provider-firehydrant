637b8e9775aba9897f4c68a538ca6844bbf11029
Alexis (AJ) Jasso <ajasso@firehydrant.io>
2025-10-07 15:24:59 -0700

    nil check all the data things

--- provider/incident_type_data.go
+++ provider/incident_type_data.go
@@ -26,75 +26,75 @@ func dataSourceIncidentType() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			// "template": {
-			// 	Type:     schema.TypeList, // Using TypeList to simulate a map
-			// 	Computed: true,
-			// 	Elem: &schema.Resource{
-			// 		Schema: map[string]*schema.Schema{
-			// 			"description": {
-			// 				Type:     schema.TypeString,
-			// 				Optional: true,
-			// 			},
-			// 			"customer_impact_summary": {
-			// 				Type:     schema.TypeString,
-			// 				Optional: true,
-			// 			},
-			// 			"severity_slug": {
-			// 				Type:     schema.TypeString,
-			// 				Optional: true,
-			// 			},
-			// 			"priority_slug": {
-			// 				Type:     schema.TypeString,
-			// 				Optional: true,
-			// 			},
-			// 			"private_incident": {
-			// 				Type:     schema.TypeBool,
-			// 				Optional: true,
-			// 			},
-			// 			// "labels": {
-			// 			// 	Type:     schema.TypeMap,
-			// 			// 	Optional: true,
-			// 			// },
-			// 			"tags": {
-			// 				Type:     schema.TypeList,
-			// 				Optional: true,
-			// 				Elem: &schema.Schema{
-			// 					Type: schema.TypeString,
-			// 				},
-			// 			},
-			// 			"runbook_ids": {
-			// 				Type:     schema.TypeList,
-			// 				Optional: true,
-			// 				Elem: &schema.Schema{
-			// 					Type: schema.TypeString,
-			// 				},
-			// 			},
-			// 			"team_ids": {
-			// 				Type:     schema.TypeList,
-			// 				Optional: true,
-			// 				Elem: &schema.Schema{
-			// 					Type: schema.TypeString,
-			// 				},
-			// 			},
-			// 			"impacts": {
-			// 				Type:     schema.TypeList,
-			// 				Optional: true,
-			// 				Elem: &schema.Resource{
-			// 					Schema: map[string]*schema.Schema{
-			// 						"impact_id": {
-			// 							Type:     schema.TypeString,
-			// 							Required: true,
-			// 						},
-			// 						"condition_id": {
-			// 							Type:     schema.TypeString,
-			// 							Required: true,
-			// 						},
-			// 					},
-			// 				},
-			// 			},
-			// 		},
-			// 	},
-			// },
+			"template": {
+				Type:     schema.TypeList, // Using TypeList to simulate a map
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"description": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"customer_impact_summary": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"severity_slug": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"priority_slug": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"private_incident": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						// "labels": {
+						// 	Type:     schema.TypeMap,
+						// 	Optional: true,
+						// },
+						"tags": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"runbook_ids": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"team_ids": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"impacts": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"impact_id": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"condition_id": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -112,52 +112,82 @@ func readDataIncidentType(ctx context.Context, d *schema.ResourceData, m interfa
 		return diag.FromErr(err)
 	}
 
-	// template := map[string]interface{}{
-	// 	"description":             *response.Template.Description,
-	// 	"customer_impact_summary": *response.Template.CustomerImpactSummary,
-	// 	"severity_slug":           *response.Template.Severity,
-	// 	"priority_slug":           *response.Template.Priority,
-	// 	"private_incident":        *response.Template.PrivateIncident,
-	// }
+	var templateSlice []map[string]interface{}
 
-	// // labels is in the sdk as an empty struct, which seems... wrong.  I'm going to implement the rest of this without it
-	// // (because I can only hold so much complexity in my head), and then investigate this from the API side to see if
-	// // this is being generated correctly.
+	if response.Template != nil {
+		var description, customerImpactSummary, severity, priority string
+		var privateIncident bool
 
-	// var tags []interface{}
-	// for _, tag := range response.Template.TagList {
-	// 	tags = append(tags, tag)
-	// }
-	// template["tags"] = tags
+		if response.Template.Description != nil {
+			description = *response.Template.Description
+		}
+		if response.Template.CustomerImpactSummary != nil {
+			customerImpactSummary = *response.Template.CustomerImpactSummary
+		}
+		if response.Template.Severity != nil {
+			severity = *response.Template.Severity
+		}
+		if response.Template.Priority != nil {
+			priority = *response.Template.Priority
+		}
+		if response.Template.PrivateIncident != nil {
+			privateIncident = *response.Template.PrivateIncident
+		}
 
-	// var runbookIDs []interface{}
-	// for _, r := range response.Template.RunbookIds {
-	// 	runbookIDs = append(runbookIDs, r)
-	// }
-	// template["runbook_ids"] = runbookIDs
+		template := map[string]interface{}{
+			"description":             description,
+			"customer_impact_summary": customerImpactSummary,
+			"severity_slug":           severity,
+			"priority_slug":           priority,
+			"private_incident":        privateIncident,
+		}
 
-	// var teamIDs []interface{}
-	// for _, team := range response.Template.TeamIds {
-	// 	teamIDs = append(teamIDs, team)
-	// }
-	// template["team_ids"] = teamIDs
+		// labels is in the sdk as an empty struct, which seems... wrong.  I'm going to implement the rest of this without it
+		// (because I can only hold so much complexity in my head), and then investigate this from the API side to see if
+		// this is being generated correctly.
 
-	// var impacts []map[string]interface{}
-	// for _, im := range response.Template.Impacts {
-	// 	impact := map[string]interface{}{
-	// 		"impact_id":    im.ID,
-	// 		"condition_id": im.ConditionID,
-	// 	}
-	// 	impacts = append(impacts, impact)
-	// }
-	// template["impacts"] = impacts
+		var tags []interface{}
+		for _, tag := range response.Template.TagList {
+			tags = append(tags, tag)
+		}
+		template["tags"] = tags
 
-	// templateSlice := []map[string]interface{}{template}
+		var runbookIDs []interface{}
+		for _, r := range response.Template.RunbookIds {
+			runbookIDs = append(runbookIDs, r)
+		}
+		template["runbook_ids"] = runbookIDs
+
+		var teamIDs []interface{}
+		for _, team := range response.Template.TeamIds {
+			teamIDs = append(teamIDs, team)
+		}
+		template["team_ids"] = teamIDs
+
+		var impacts []map[string]interface{}
+		for _, im := range response.Template.Impacts {
+			impact := map[string]interface{}{
+				"impact_id":    im.ID,
+				"condition_id": im.ConditionID,
+			}
+			impacts = append(impacts, impact)
+		}
+		template["impacts"] = impacts
+
+		templateSlice = append(templateSlice, template)
+	}
+	var name, description string
+	if response.Name != nil {
+		name = *response.Name
+	}
+	if response.Description != nil {
+		description = *response.Description
+	}
 
 	attributes := map[string]interface{}{
-		"name":        *response.Name,
-		"description": *response.Description,
-		// "template":    templateSlice,
+		"name":        name,
+		"description": description,
+		"template":    templateSlice,
 	}
 
 	for key, value := range attributes {
--- provider/incident_type_data_test.go
+++ provider/incident_type_data_test.go
@@ -44,32 +44,32 @@ func TestAccIncidentTypeDataSource_allAttributes(t *testing.T) {
 						"data.firehydrant_incident_type.test_incident_type", "name", fmt.Sprintf("test-incident-type-%s", rName)),
 					resource.TestCheckResourceAttr(
 						"data.firehydrant_incident_type.test_incident_type", "description", fmt.Sprintf("test-description-%s", rName)),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.description", "test-template-description"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.customer_impact_summary", "test-summary"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.severity_slug", "SEV1"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.priority_slug", "TESTPRIORITY"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.private_incident", "false"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.tags.0", "foo"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.tags.1", "bar"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.runbook_ids.0", "88f9f172-cc07-477e-9a80-b1ae7669ec3d"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.runbook_ids.1", "39de1363-4ae3-4aa3-913b-d63312c76afd"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.team_ids.0", "cd2b6d18-e616-4990-9065-ec0cab037680"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.team_ids.1", "2af88e28-0205-4d39-b304-133bd2e19358"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.impacts.0.impact_id", "500d9e2e-ea7c-4834-a81f-e336de24dbb1"),
-					// resource.TestCheckResourceAttr(
-					// 	"data.firehydrant_incident_type.test_incident_type", "template.0.impacts.0.condition_id", "99762c0c-1ee0-44a0-a3a7-d1316dd902ca"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.description", "test-template-description"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.customer_impact_summary", "test-summary"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.severity_slug", "SEV1"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.priority_slug", "TESTPRIORITY"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.private_incident", "false"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.tags.0", "foo"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.tags.1", "bar"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.runbook_ids.0", "88f9f172-cc07-477e-9a80-b1ae7669ec3d"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.runbook_ids.1", "39de1363-4ae3-4aa3-913b-d63312c76afd"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.team_ids.0", "cd2b6d18-e616-4990-9065-ec0cab037680"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.team_ids.1", "2af88e28-0205-4d39-b304-133bd2e19358"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.impacts.0.impact_id", "500d9e2e-ea7c-4834-a81f-e336de24dbb1"),
+					resource.TestCheckResourceAttr(
+						"data.firehydrant_incident_type.test_incident_type", "template.0.impacts.0.condition_id", "99762c0c-1ee0-44a0-a3a7-d1316dd902ca"),
 				),
 			},
 		},
@@ -108,7 +108,7 @@ resource "firehydrant_incident_type" "test_incident_type" {
 		tags = [ "foo", "bar" ]
 		runbook_ids = [ "88f9f172-cc07-477e-9a80-b1ae7669ec3d", "39de1363-4ae3-4aa3-913b-d63312c76afd" ]
 		team_ids = [ "cd2b6d18-e616-4990-9065-ec0cab037680", "2af88e28-0205-4d39-b304-133bd2e19358" ]
-
+		
 		impacts {
 		    impact_id = "500d9e2e-ea7c-4834-a81f-e336de24dbb1"
 				condition_id = "99762c0c-1ee0-44a0-a3a7-d1316dd902ca"
